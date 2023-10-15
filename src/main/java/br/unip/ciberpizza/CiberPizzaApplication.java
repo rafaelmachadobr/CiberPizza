addbd52afb21e6b4b357c232db267056d292cb57
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-15 11:56:11 -0300

    Criando projeto

---
+++ src/main/java/br/unip/ciberpizza/CiberPizzaApplication.java
@@ -0,0 +1,13 @@
+package br.unip.ciberpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiberPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiberPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unip/ciberpizza/CiberPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unip.ciberpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiberPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
