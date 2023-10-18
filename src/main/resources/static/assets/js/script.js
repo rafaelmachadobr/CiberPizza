8c967450448c8f4b9a6555bd5f8e061da2e1eb45
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-18 16:07:00 -0300

    Assets

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -116,4 +116,9 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  $(document).ready(function () {
+    $(".cpf").mask("000.000.000-00", { reverse: true });
+    $("#telefone").mask("(00) 00000-0000");
+  });
 });
